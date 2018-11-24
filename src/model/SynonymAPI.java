da74d6db1d2fa6b6104736ae08aae7f997f33e2e
Nicole Anderson-Au <nvaa36@gmail.com>
2018-11-23 23:05:43 -0800

    Fix code smells

--- src/model/SynonymAPI.java
+++ src/model/SynonymAPI.java
@@ -3,9 +3,11 @@ package model;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
+import java.lang.String;
+import java.lang.StringBuilder;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -23,21 +25,19 @@ public class SynonymAPI {
         try {
             datamuse = new URL("http://api.datamuse.com/words?rel_syn=" + word);
             dc = datamuse.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(dc.getInputStream(), "UTF-8"));
+            BufferedReader in = new BufferedReader(new InputStreamReader(dc.getInputStream(), StandardCharsets.UTF_8));
             String inputLine;
             while ((inputLine = in.readLine()) != null)
                 str.append(inputLine);
             in.close();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return str != null ? synonymArray(str.toString()) : null;
+        return str.length() > 0 ? synonymArray(str.toString()) : null;
 	}
 	
 	public List<String> synonymArray(String synStr) {
-		List<String> synonyms = new ArrayList<String>();
+		List<String> synonyms = new ArrayList<>();
 		
 		JsonParserFactory factory=JsonParserFactory.getInstance();
         JSONParser parser=factory.newJsonParser();
