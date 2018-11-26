989a7dc4f18900deec2ef1c42134f857f629b218
Stephanie <stephcarpin710@gmail.com>
2018-11-26 14:54:37 -0800

    update resume and settings exit
    menu with settings and levels

---
+++ src/controller/MenuPage.java
@@ -0,0 +1,81 @@
+package controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
+
+public class MenuPage {
+	
+	@FXML
+	private Button mainMenuButton;
+	
+	//make settings return and levels 
+	@FXML
+    private Button settingsButton;
+	@FXML
+    private Button levelButton;
+
+	
+	public void gotoMainMenu(ActionEvent actionEvent) {
+		Window owner = mainMenuButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/LoginPage.fxml"));
+        Parent root = null;
+        try {
+        	root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage = (Stage) owner;
+        Scene scene = null;
+        scene = new Scene(root);
+        stage.setScene(scene);
+	}
+	
+	/**
+     * Default action for opening the settings.
+     * @param actionEvent
+     */
+    public void openSettings(ActionEvent actionEvent) {
+    	Window owner = settingsButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/SettingsPage.fxml"));
+        Parent root = null;
+        try {
+        	root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage = (Stage) owner;
+        Scene scene = null;
+        scene = new Scene(root);
+        stage.setScene(scene);
+    }
+    
+    /**
+     * Default action for choosing level.
+     * @param actionEvent
+     */
+    public void getLevel(ActionEvent actionEvent) {
+    	Window owner = levelButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/difficulty.fxml"));
+        Parent root = null;
+        try {
+        	root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage = (Stage) owner;
+        Scene scene = null;
+        scene = new Scene(root);
+        stage.setScene(scene);
+    }
+    
+	
+}
