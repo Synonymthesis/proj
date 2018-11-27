f9a3026cb05738aad7bd1088ec68297be1b52dd0
Stephanie <stephcarpin710@gmail.com>
2018-11-26 16:00:25 -0800

    add settings and level button

--- src/controller/MenuPage.java
+++ src/controller/MenuPage.java
@@ -17,7 +17,13 @@ public class MenuPage {
 	@FXML
 	private Button mainMenuButton;
 	
-	public void gotoMainMenu(ActionEvent actionEvent) {
+	//make settings return and levels 
+	@FXML
+	private Button settingsButton;
+	@FXML
+	private Button levelButton;
+	
+	public void gotoMainMenu() {
 		Window owner = mainMenuButton.getScene().getWindow();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/LoginPage.fxml"));
         Parent root = null;
@@ -31,4 +37,42 @@ public class MenuPage {
         scene = new Scene(root);
         stage.setScene(scene);
 	}
+	
+	/**
+     * Default action for opening the settings.
+     * @param actionEvent
+     */
+    public void openSettings() {
+    	Window owner = settingsButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/SettingsPage.fxml"));
+        Parent root = null;
+        try {
+        	root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage = (Stage) owner;
+        Scene scene = null;
+        scene = new Scene(root);
+        stage.setScene(scene);
+    }
+    
+    /**
+     * Default action for choosing level.
+     * @param actionEvent
+     */
+    public void getLevel() {
+    	Window owner = levelButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/difficulty.fxml"));
+        Parent root = null;
+        try {
+        	root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage = (Stage) owner;
+        Scene scene = null;
+        scene = new Scene(root);
+        stage.setScene(scene);
+    }
 }
