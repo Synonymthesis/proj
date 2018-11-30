62395894d7aaafeb6287ce103146f601989bb210
Nicole Anderson-Au <nvaa36@gmail.com>
2018-11-30 14:27:15 -0800

    Add more leaderboard test

--- src/test/LeaderboardDatabaseTest.java
+++ src/test/LeaderboardDatabaseTest.java
@@ -83,5 +83,37 @@ public class LeaderboardDatabaseTest {
 		LeaderboardDatabase data2 = new LeaderboardDatabase(FILENAME);
 		assertEquals(scores, data2.getScores());
 	}
+	
+	@Test
+	public void testWriteManyScores() {
+		Map<String, Integer> scores = new HashMap<>();
+		String name1 = "Test1";
+		int score1 = 123;
+		String name2 = "Test2";
+		int score2 = 456;
+		String name3 = "Test3";
+		int score3 = 789;
+		
+		data.saveScore(name1, score1);
+		assertFalse(data.getScores().isEmpty());
+		
+		scores.put(name1, score1);
+		assertEquals(scores, data.getScores());
+		
+		data.saveScore(name2, score2);
+		assertFalse(data.getScores().isEmpty());
+		
+		scores.put(name2, score2);
+		assertEquals(scores, data.getScores());
+		
+		LeaderboardDatabase data2 = new LeaderboardDatabase(FILENAME);
+		assertEquals(scores, data2.getScores());
+		
+		data2.saveScore(name3, score3);
+		assertFalse(data2.getScores().isEmpty());
+		
+		scores.put(name3, score3);
+		assertEquals(scores, data2.getScores());
+	}
 
 }
