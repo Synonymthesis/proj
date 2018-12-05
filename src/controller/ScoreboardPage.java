12394efe203cd572a09e47902a73e6bb504432dc
kschandr <mykaysha@gmail.com>
2018-12-05 12:53:01 -0800

    Revert "fix vulnerability"
    This reverts commit bac0f842ccc211c20c3660d5a8ba44b273d27ab3.

--- src/controller/ScoreboardPage.java
+++ src/controller/ScoreboardPage.java
@@ -1,10 +1,17 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import model.Player;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -27,18 +34,33 @@ public class ScoreboardPage implements Initializable {
 	}
 	
 	public void backToMain() {
-		if (LoginPage.getNextScene()=="Scoreboard") {
-			SceneController controller = new SceneController();
-	        controller.transitionScene(backButton, "../view/LoginPage.fxml");
+		LoginPage l = new LoginPage();
+		EndGame e = new EndGame();
+		if (l.getNextScene()=="Scoreboard") {
+			transitionScene(backButton, "../view/LoginPage.fxml");
 		} 
-		else if (EndGame.getNextScene()=="Scoreboard"){
-			SceneController controller = new SceneController();
-	        controller.transitionScene(backButton, "../view/EndGame.fxml");
+		else if (e.getNextScene()=="Scoreboard"){
+			transitionScene(backButton, "../view/EndGame.fxml");
 		}
 		else {
-			SceneController controller = new SceneController();
-	        controller.transitionScene(backButton,"../view/PlayGame.fxml");
+			transitionScene(backButton, "../view/PlayGame.fxml");
 		}
+	}
+	
+	private void transitionScene(Button button, String fxmlScene) {
+		Window owner = button.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlScene));
+        Parent root = null;
+        try {
+        	loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        root = loader.getRoot();
+        Stage stage = (Stage) owner;
+        Scene scene = null;
+        scene = new Scene(root);
+        stage.setScene(scene);
 	}
 	
 
