ead5b6c27cfe3316938c227c1e8b8d19e7d4fb99
ahak0 <a_hak0@yahoo.com>
2018-12-04 18:17:07 -0800

    attempting to fix duplicate code block smells

---
+++ src/controller/SceneController.java
@@ -0,0 +1,36 @@
+package controller;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+public class SceneController {
+
+	private static final Logger LOGGER = Logger.getLogger(LoginPage.class.getName());
+	
+	public SceneController() {
+		
+	}
+	
+	public void transitionScene(Button button, String fxmlScene) {
+		Window owner = button.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlScene));
+        Parent root = null;
+        try {
+        	root = loader.load();
+        } catch (IOException e) {
+            LOGGER.log(Level.WARNING, "Button won't work.");
+        }
+        Stage stage = (Stage) owner;
+        Scene scene = null;
+        scene = new Scene(root);
+        stage.setScene(scene);
+	}
+}
